b5d932ea1c8e7447bc5027a323a2fd8be4c9e219
Alvin Hsia <alvinhsia@gmail.com>
2016-02-16 22:26:03 -0800

    Initial Commit

---
+++ HW 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW 3
+//
+//  Created by Alvin Hsia on 2/16/16.
+//  Copyright © 2016 Alvin Hsia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
